573429a6ecbaecd15795f7417c619315bc975bfd
believeme <cain1227@naver.com>
2025-09-21 00:57:01 +0900

    lesson: 2-8 @ManyToOne 양방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -1,8 +1,11 @@
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+        import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,4 +16,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "user") // 외래키 주인의 필드를 mappedBy 값으로 지정(Food클래스의 user필드)
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // 외래 키(연관 관계) 설정
+    }
 }
