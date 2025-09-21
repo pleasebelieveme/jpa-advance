ed94bd4d551932b16ba4ee49e1321a0c23dc9ca0
believeme <cain1227@naver.com>
2025-09-21 17:21:49 +0900

    lesson: 2-10 @ManyToMany 단방향 테스트

--- src/test/java/com/sparta/jpaadvance/relation/ManyToManyTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/ManyToManyTest.java
@@ -11,11 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Transactional
 @SpringBootTest
-public class OneToManyTest {
+public class ManyToManyTest {
 
     @Autowired
     UserRepository userRepository;
@@ -24,8 +22,9 @@ public class OneToManyTest {
 
     @Test
     @Rollback(value = false)
-    @DisplayName("1대N 단방향 테스트")
+    @DisplayName("N대M 단방향 테스트")
     void test1() {
+
         User user = new User();
         user.setName("Robbie");
 
@@ -35,27 +34,13 @@ public class OneToManyTest {
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-        food.getUserList().add(user); // 외래 키(연관 관계) 설정
-        food.getUserList().add(user2); // 외래 키(연관 관계) 설정
+        food.getUserList().add(user);
+        food.getUserList().add(user2);
 
         userRepository.save(user);
         userRepository.save(user2);
         foodRepository.save(food);
 
-        // 추가적인 UPDATE 쿼리 발생을 확인할 수 있습니다.
+        // 자동으로 중간 테이블 orders 가 create 되고 insert 됨을 확인할 수 있습니다.
     }
-
-    @Test
-    @DisplayName("1대N 조회 테스트")
-    void test2() {
-        Food food = foodRepository.findById(1L).orElseThrow(NullPointerException::new);
-        System.out.println("food.getName() = " + food.getName());
-
-        // 해당 음식을 주문한 고객 정보 조회
-        List<User> userList = food.getUserList();
-        for (User user : userList) {
-            System.out.println("user.getName() = " + user.getName());
-        }
-    }
-
 }
